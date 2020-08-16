6159774668efebd2f0ef135079cfb105220d8c86
karan kaul <karankaul10@gmail.com>
2020-08-17 00:02:08 +0530

    > Added Github and Linkedin profile links to the homepage. > Added input value suggestions ( in the labels ).

--- forms.py
+++ forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired
 
 # create fields for each variable 
 class Inputform(FlaskForm):
-    sl = FloatField('SEPAL LENGTH',validators=[DataRequired()])
-    sw = FloatField('SEPAL WIDTH',validators=[DataRequired()])
-    pl = FloatField('PETAL LENGTH',validators=[DataRequired()])
-    pw = FloatField('PETAL WIDTH',validators=[DataRequired()])
+    sl = FloatField('SEPAL LENGTH (~ 4.3 - 7.9)',validators=[DataRequired()])
+    sw = FloatField('SEPAL WIDTH (~ 2.0 - 4.4)',validators=[DataRequired()])
+    pl = FloatField('PETAL LENGTH (~ 1.0 - 6.9)',validators=[DataRequired()])
+    pw = FloatField('PETAL WIDTH (~ 0.1 - 2.5)',validators=[DataRequired()])
     submit = SubmitField('PREDICT')
